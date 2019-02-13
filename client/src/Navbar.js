8cfd16f3b06cf2691cf51db0b49a1f59351a780e
Francisco Garcia <francisco@simplexrex.com>
2019-02-13 13:07:43 -0700

    added navbar

---
+++ client/src/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+function Navbar(props) {
+    return (
+        <nav className="navbar-wrapper">
+            <div className="nav-link">
+                <Link to="/signup">Sign Up</Link>
+            </div>
+            <div className="nav-link">
+                <Link to="/login">Log In</Link>
+            </div>
+            <div className="nav-link">
+                <Link to="/Procedures">Procedures</Link>
+            </div>
+            <div className="nav-link">
+                <button onClick={() => props.logout()}>Logout</button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
