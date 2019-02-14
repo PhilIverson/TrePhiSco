a1ba71f4fba4f5852103b690db5161642b67e328
Francisco Garcia <francisco@simplexrex.com>
2019-02-14 16:08:41 -0700

    added search filter to Search

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,21 +1,38 @@
-import React from 'react'
-
-function Search() {
-    const procedures = props.procedures.map(procedure => {
-        return (
-            <Procedure
-                key={procedure._id}
-                procedure={procedure}
-                savedProcedure={props.savedProcedure}
-            />
-        )
-    })
+import React, {Component} from 'react'
+import SearchInput, {createFilter} from 'react-search-input'
+ 
+import {procedureContext} from './procedureProvider'
+ 
+const KEYS_TO_FILTERS = ['user.name', 'subject', 'dest.name']
+ 
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
+ 
+  render () {
+    const filteredProcedures = procedures.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+ 
     return (
-        <div>
-            {procedures}
-        </div>
+      <div>
+        <SearchInput className="search-input" onChange={this.searchUpdated} />
+        {filteredProcedures.map(procedure => {
+          return (
+            <div className="mail" key={procedure.id}>
+              <div className="from">{procedure.description}</div>
+              <div className="subject">{procedure.price}</div>
+            </div>
+          )
+        })}
+      </div>
     )
+  }
+ 
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
 }
-
-export default Search
-
