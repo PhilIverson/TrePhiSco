e0ec952a83f09bfa8af14a9dc3995e754cbd053e
Trevor Steinle <trevorsteinle@gmail.com>
2019-02-18 14:14:39 -0700

    vanilla state

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import { withSearchContext } from './SearchProvider';
 function App(props) {
   return (
     <div className="app-wrapper">
+      <SearchProvider>
         <SearchForm />
+      </SearchProvider>
     </div>
   )
 }
